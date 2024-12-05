8eb253b350a3c7255ac6f5efafaa90e3b4802664
Kershjan <k.jump35@gmail.com>
2024-12-05 16:59:15 +0400

    Add files via upload

---
+++ modul_2_2.py
@@ -0,0 +1,9 @@
+first = 10
+second = 15
+third = 10
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
